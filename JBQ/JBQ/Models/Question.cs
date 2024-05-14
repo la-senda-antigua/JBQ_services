e4b54c40f6e6d246da82454497d850e2deabab16
hugo.quinonez <hugo.quinonez@imperialtex.com>
2024-05-14 10:17:26 -0600

    create question controller

---
+++ JBQ/JBQ/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JBQ.Models
+{
+    public class Question
+    {
+        public Int64 PK_Question_ID { get; set; }
+        public string QuestionText { get; set; }
+        public string AnswerText { get; set; }
+        public string Reference { get; set; }
+        public Int64 Points { get; set; }
+        public Int64 UniqueCharacterIndex { get; set; }
+    }
+}
